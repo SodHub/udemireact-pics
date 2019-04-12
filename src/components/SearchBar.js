68c3df9c9516e94fd9096e746ea7c4c29680f518
Slovinskaya Olga <slo@ciklum.vom>
2019-04-12 16:06:55 +0300

    App Structure

---
+++ src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+class SearchBar extends React.Component  {
+
+    onInputChange (event) {
+        event.target.value
+    }
+
+    render () {
+        return (
+            <div className="ui segment">
+              <form className="ui form">
+                  <div className="ui field">
+                    <label> Image Search:</label>
+                    <input type="text" onChange={this.onInputChange}/>
+                  </div>
+              </form>
+            </div>
+        );
+    }
+
+
+};
+
+export default SearchBar;
